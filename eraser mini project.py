e56f4e2ee2135dbf31d1faabb6b9e82677b5e261
meghana499 <43784392+meghana499@users.noreply.github.com>
2019-07-23 00:22:15 +0530

    Create eraser mini project.py
    A simple eraser like tool created by using OpenCV library in Python

---
+++ eraser mini project.py
@@ -0,0 +1,31 @@
+import cv2
+
+drawing = False 
+ix,iy = -1,-1
+
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    global ix,iy,drawing
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+                cv2.rectangle(img,(ix,iy),(x,y),(255,255,255),-1)          
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False     
+
+img=cv2.imread("D:/lenovo laptop data/Pictures/book.jpg")
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_circle)
+
+while(1):
+    cv2.imshow('image',img)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
